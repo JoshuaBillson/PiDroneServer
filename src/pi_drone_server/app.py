3086c60e6935d93cdb38e335282d5c775feab7c5
Joshua Billson <jmbillson@outlook.com>
2020-08-27 20:01:12 +0000

    Refactored app.py

--- src/pi_drone_server/app.py
+++ src/pi_drone_server/app.py
@@ -1,74 +1,20 @@
+# Ros Client
 import rospy
-from geometry_msgs.msg import Twist
-from flask import Flask, request
+
+# Standard Python Libraries
 import threading
 import os
 
-html = """<!DOCTYPE html>
-<html lang="en" dir="ltr">
+# Messages
+from geometry_msgs.msg import Twist
 
-<head>
-    <!-- Meta Data -->
-    <meta charset="utf-8">
-    <meta name="author" content="Joshua Billson">
-    <meta name="description" content="Robot Server">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+# Third Party Libraries
+from flask import Flask, request
+from pi_drone_server.html import html
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <title>Robot Server</title>
-</head>
-
-<body>
-<div class="container-fluid my-0 mt-md-3 mx-0 py-0 pt-md-3 px-0 mb-0 pb-0">
-    <div class="row align-content-center text-center mx-0">
-        <div class="col-10 offset-1 col-md-8 offset-md-2">
-            <img class="img img-fluid" src="https://cdn.pixabay.com/photo/2019/11/10/16/47/nature-4616282_1280.jpg"
-                 alt="viewport" style="max-height: 80vh;">
-        </div>
-        <div class="col-12 offset-md-2 col-md-8 py-1 my-1 align-content-center text-center">
-            <button onclick="turnLeft()" class="btn btn-dark btn-large py-2 m-2" style="width: 100px;">Left</button>
-            <button onclick="forward()" class="btn btn-dark btn-large py-2 m-2" style="width: 100px;">Forward</button>
-            <button onclick="backward()" class="btn btn-dark btn-large py-2 m-2" style="width: 100px;">Reverse</button>
-            <button onclick="turnRight()" class="btn btn-dark btn-large py-2 m-2" style="width: 100px;">Right</button>
-        </div>
-    </div>
-</div>
-
-<!-- Scripts -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
-<script>
-    let request = new XMLHttpRequest();
-
-    function turnLeft() {
-        request.open("GET", "/control?speed=0&turn=1");
-        request.send();
-    }
-
-    function turnRight() {
-        request.open("GET", "/control?speed=0&turn=-1");
-        request.send();
-    }
-
-    function forward() {
-        request.open("GET", "/control?speed=1&turn=0");
-        request.send();
-    }
-
-    function backward() {
-        request.open("GET", "/control?speed=-1&turn=0");
-        request.send();
-    }
-</script>
-
-</body>
-
-</html>"""
-
+# Globals
+current_speed = 0
+current_turn = 0
 app = Flask(__name__)
 
 threading.Thread(target=lambda: rospy.init_node('pi_drone_server', disable_signals=True)).start()
@@ -81,18 +27,26 @@ def view():
 
 @app.route("/control")
 def control():
-    global direction
-    speed = 0
-    turn = 0
-    if 'speed' in request.args:
-        speed = int(request.args['speed'])
-    if 'turn' in request.args:
-        turn = int(request.args['turn'])
+    global direction, current_speed, current_turn
+    # Initialize Message
     msg = Twist()
-    msg.linear.x = speed
-    msg.angular.z = turn
+    msg.linear.x = 0
+    msg.angular.z = 0
+
+    # Decode Request
+    if 'speed' in request.args and int(request.args["speed"]) != current_speed:
+        msg.linear.x = int(request.args["speed"])
+    if 'turn' in request.args and int(request.args["turn"]) != current_turn:
+        msg.angular.z = int(request.args["turn"])
+
+    # Update Current Speed and Turn
+    current_speed = msg.linear.x
+    current_turn = msg.angular.z
+
+    # Start a New Thread To Publish The Twist Message
     threading.Thread(target=lambda: direction.publish(msg)).start()
-    #direction.Publish(msg)
+
+    # Return Code 204
     return ('', 204)
 
 
---
+++ src/pi_drone_server/html.py
@@ -0,0 +1,64 @@
+html = """<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <!-- Meta Data -->
+    <meta charset="utf-8">
+    <meta name="author" content="Joshua Billson">
+    <meta name="description" content="Robot Server">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Robot Server</title>
+</head>
+
+<body>
+<div class="container-fluid my-0 mt-md-3 mx-0 py-0 pt-md-3 px-0 mb-0 pb-0">
+    <div class="row align-content-center text-center mx-0">
+        <div class="col-10 offset-1 col-md-8 offset-md-2">
+            <img class="img img-fluid" src="https://cdn.pixabay.com/photo/2019/11/10/16/47/nature-4616282_1280.jpg"
+                 alt="viewport" style="max-height: 80vh;">
+        </div>
+        <div class="col-12 offset-md-2 col-md-8 py-1 my-1 align-content-center text-center">
+            <button onclick="turnLeft()" class="btn btn-dark btn-large py-2 m-2" style="width: 100px;">Left</button>
+            <button onclick="forward()" class="btn btn-dark btn-large py-2 m-2" style="width: 100px;">Forward</button>
+            <button onclick="backward()" class="btn btn-dark btn-large py-2 m-2" style="width: 100px;">Reverse</button>
+            <button onclick="turnRight()" class="btn btn-dark btn-large py-2 m-2" style="width: 100px;">Right</button>
+        </div>
+    </div>
+</div>
+
+<!-- Scripts -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<script>
+    let request = new XMLHttpRequest();
+
+    function turnLeft() {
+        request.open("GET", "/control?speed=0&turn=1");
+        request.send();
+    }
+
+    function turnRight() {
+        request.open("GET", "/control?speed=0&turn=-1");
+        request.send();
+    }
+
+    function forward() {
+        request.open("GET", "/control?speed=1&turn=0");
+        request.send();
+    }
+
+    function backward() {
+        request.open("GET", "/control?speed=-1&turn=0");
+        request.send();
+    }
+</script>
+
+</body>
+
+</html>"""
